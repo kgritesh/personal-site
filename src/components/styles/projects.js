68920bf6fcc22edd1d1273fdf4e81b1743e2b10e
sagar Dhandhukiya <sagar.dhandhukiya@propalius.com>
2023-10-25 11:54:00 +0530

    changes in css

--- src/components/styles/projects.js
+++ src/components/styles/projects.js
@@ -5,57 +5,69 @@ import { colors } from '../../utils/theme';
 export const StyledProject = styled.div`
   animation-name: fadeIn;
   animation-duration: 0.5s;
-    border:1px solid transparent;
-    padding: 10px 15px;
-    border-radius: 8px;
-    cursor: pointer;
-    a{
-      text-decoration:none;
-    }
-  h3,p {
-    // margin-bottom: ${rhythm(1 / 4)};
-    margin:0;
+  border: 1px solid transparent;
+  padding: 10px 15px;
+  border-radius: 8px;
+  cursor: pointer;
+
+  a {
+    text-decoration: none;
   }
+
+  h3,
+  p {
+    margin: 0;
+  }
+
   h3 {
-width:fit-content;    display: flex;
+    width: fit-content;
+    display: flex;
     align-items: center;
-    gap:5px;
+    gap: 5px;
     flex-direction: row;
-}
-    .arrow{
-      display : inline ;
-    }}
+  }
+
+  .arrow {
+    display: inline;
+  }
 
   small {
     color: ${colors.blue};
   }
+
   h3:hover {
-    
     text-decoration: underline;
     text-decoration-color: ${colors.red};
-    .arrow{
-      display : inline;
-    }}
+
+    .arrow {
+      display: inline;
+    }
+  }
+
   :hover {
-    border:1px solid ${colors.lightBlack};
+    border: 1px solid ${colors.lightBlack};
     border-radius: 8px;
   }
+
   @media (max-width: 800px) {
-  h3{
-    border-bottom:2px solid black;
-  }
+    h3 {
+      border-bottom: 2px solid black;
+    }
   }
 `;
 
 export const StyledArticle = styled.div`
   animation-name: fadeIn;
   animation-duration: 0.5s;
+
   h3 {
     margin-bottom: ${rhythm(1 / 4)};
   }
+
   p {
     margin-bottom: 0;
   }
+
   small {
     color: ${colors.blue};
     margin-bottom: ${rhythm(1 / 2)};
@@ -65,6 +77,7 @@ export const StyledArticle = styled.div`
 export const StyledSummary = styled.p`
   color: ${colors.palered};
 `;
+
 export const PostWrap = styled.div`
   display: flex;
   flex-direction: column;
